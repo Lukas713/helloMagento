10066139aa4706b5abbe96806855fdc4a5c65ae4
Lukas <lukas.scharmitzer@gmail.com>
2019-05-10 15:25:17 +0200

    escape html when inserting news

--- app/code/Inchoo/Sample06/Controller/Adminhtml/News/Insert.php
+++ app/code/Inchoo/Sample06/Controller/Adminhtml/News/Insert.php
@@ -4,6 +4,7 @@ namespace Inchoo\Sample06\Controller\Adminhtml\News;
 
 use Inchoo\Sample04\Api\Data\NewsInterface;
 use Magento\Backend\App\Action;
+use Magento\Framework\Escaper;
 use Magento\Framework\Exception\CouldNotSaveException;
 use Magento\Framework\Message\Manager;
 use Magento\Framework\View\Result\PageFactory;
@@ -25,6 +26,10 @@ class Insert extends Action
      * @var \Inchoo\Sample04\Api\Data\NewsInterfaceFactory
      */
     protected $newsModelFactory;
+    /**
+     * @var Escaper
+     */
+    private $escaper;
 
     public function __construct
     (
@@ -32,7 +37,8 @@ class Insert extends Action
         Manager $manager,
         PageFactory $pageFactory,
         \Inchoo\Sample04\Api\NewsRepositoryInterface $newsRepository,
-        \Inchoo\Sample04\Api\Data\NewsInterfaceFactory $newsModelFactory
+        \Inchoo\Sample04\Api\Data\NewsInterfaceFactory $newsModelFactory,
+        Escaper $escaper
     )
     {
         parent::__construct($context);
@@ -40,14 +46,18 @@ class Insert extends Action
         $this->pageFactory = $pageFactory;
         $this->newsRepository = $newsRepository;
         $this->newsModelFactory = $newsModelFactory;
+        $this->escaper = $escaper;
     }
 
     public function execute()
     {
-        $result = $this->getRequest()->getParams();
-        if($result[NewsInterface::TITLE] !== ''){
+        $params = $this->getRequest()->getParams();
+        foreach($params as $key => $value){
+            $params[$key] = $this->escaper->escapeHtml($value);
+        }
+        if($params[NewsInterface::TITLE] !== ''){
             try {
-                $this->newsRepository->addRecord($result);
+                $this->newsRepository->addRecord($params);
             }catch (CouldNotSaveException $exception){
                 var_dump($exception->getMessage());
             }
